1b54756a71e0b857cd2b673d57a8a0494f428a28
Jakub Krysmalski <jakub16@gmail.com>
2022-07-28 22:22:26 +0200

    Removed footer, set fixed width on Advice container

--- script.js
+++ script.js
@@ -3,10 +3,27 @@ var request = new XMLHttpRequest()
 let adviceID = document.getElementById('advice-ID');
 let advice = document.getElementById('advice-text');
 var dice = document.getElementById('dice');
+const transition = document.getElementById('advice-transition')
+// let transition = document.getElementsByClassName('advice-container');
 
 const API = 'https://api.adviceslip.com/advice';
 
 function displayAdvice() {
+    // transition.classList.remove('transition')
+    // transition.classList.remove('transition');
+    console.log(transition.className);
+
+    transition.classList.toggle('transition')
+    // transition.classList.remove('transition')
+    console.log(transition.className);
+    // void transition.offsetWidth;
+    // console.log(transition.className);
+    // transition.classList.remove('transition')
+    // console.log(transition.className);
+    // setTimeout(() => {
+    //     // transition.classList.add('transition');
+    //     transition.classList.toggle('transition')
+    // }, 100);
     request.open('GET', API, true);
     request.send();
     request.onload = function () {
